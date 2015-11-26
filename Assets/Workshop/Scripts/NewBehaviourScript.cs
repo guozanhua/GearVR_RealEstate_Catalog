b6a7f9a134bb48633f6ffa3b348f5214e9bff088
Grzegorz Bukowski <grzegorz-bukowski@o2.pl>
2015-11-26 16:40:48 +0100

    v0.01
    Tekstury tła/cubemapy oraz jedna sfera testowa

---
+++ Assets/Workshop/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UnityEngine.VR.VRSettings.renderScale = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
